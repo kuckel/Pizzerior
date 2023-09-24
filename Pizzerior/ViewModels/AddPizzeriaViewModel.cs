e2911f565252db6305267d41cc032139e5eeeb88
peter milered <petermilered@hotmail.com>
2023-09-24 23:00:32 +0200

    PizzeriaAdd funktion att stänga fönster

--- Pizzerior/ViewModels/AddPizzeriaViewModel.cs
+++ Pizzerior/ViewModels/AddPizzeriaViewModel.cs
@@ -1,14 +1,50 @@
-﻿using System;
+﻿#nullable disable 
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Pizzerior.Models;
+using Pizzerior.Services;
+using Pizzerior.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Pizzerior.ViewModels
 {
-    public class AddPizzeriaViewModel
+    public partial class AddPizzeriaViewModel: ObservableObject
     {
+        [ObservableProperty]
+        public Pizzeria _selectedPizzeria;
+        IPizzeriaService _pizzeriaService;
 
+        public AddPizzeriaViewModel()
+        {
+            _pizzeriaService = new PizzeriaService();
+        }
+
+
+
+        [RelayCommand]
+        void CloseWin()
+        {
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window is PizzeriaAdd)
+                {
+                    window.Close();
+                    break;
+                }
+            }
+        }
+        
+
+        [RelayCommand]
+        void Save(Pizzeria pizzeria)
+        {
+            
+        }
 
 
     }
